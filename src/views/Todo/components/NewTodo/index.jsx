be8f02b89a5527f8eead34de43c7b5a6cbc840bb
zxp_pc <1492548812@qq.com>
2023-04-13 21:55:55 +0800

    input事件学习

--- src/views/Todo/components/NewTodo/index.jsx
+++ src/views/Todo/components/NewTodo/index.jsx
@@ -1,30 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 import { Input } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 import moduleCss from './index.module.scss'
 import Icon from '@components/Icon'
 
 
+
 export default function Index() {
-  const test = (e) => {
-    console.log(e);
+  const inputRef = useRef(null)
+  const [state, setState] = useState({
+    btnMode: false
+  })
+  const [todo, setTodo] = useState({
+    content: '',
+    objTime: ''
+  })
+
+  const submit = (e) => {
+    const { current: { input: { value } } } = inputRef
+    if (!value) return
+    console.log(value);
+  }
+  const handlBlur = () => {
+    const { current: { input: { value } } } = inputRef
+    if (!value) setState({ btnMode: false })
   }
   return (
     <div className={moduleCss.newtodo} >
       <div className='left'>
         <div className='btn show-btn'>
-          <Icon type='pp-big-circle' />
+          {state.btnMode ? <Icon title='创建任务' type='pp-big-circle' onClick={submit} /> : <PlusOutlined title='添加任务' onClick={() => inputRef.current.focus({ cursor: 'end' })} />}
         </div>
         <div className='input'>
           <Input
+            ref={inputRef}
+            onFocus={() => setState({ btnMode: true })}
+            onBlur={handlBlur}
             bordered={false}
             style={{
               height: '100%',
               padding: '4px 0'
             }}
             placeholder='添加任务'
-            onPressEnter={test}
+            onPressEnter={submit}
+            max={500}
           />
         </div>
       </div>
