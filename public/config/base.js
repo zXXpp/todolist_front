3ec6f6d5f6c3a121c2a303c895369ea37c9d2e82
zhaoxuepeng <1492548812@qq.com>
2023-04-13 17:33:21 +0800

    创建todo的输入框

--- public/config/base.js
+++ public/config/base.js
@@ -1,4 +1,4 @@
 window.config = {
     baseUrl: 'http://127.0.0.1:9000',
-    iconfontCN:'//at.alicdn.com/t/c/font_3365114_5adpv71h3.js'
+    iconfontCN:'//at.alicdn.com/t/c/font_3365114_oydll55v3.js'
 }
--- src/views/Todo/components/NewTodo/index.jsx
+++ src/views/Todo/components/NewTodo/index.jsx
@@ -1,13 +1,32 @@
 import React, { useState } from 'react'
+
+import { Input } from 'antd';
+
 import moduleCss from './index.module.scss'
+import Icon from '@components/Icon'
+
 
 export default function Index() {
-
+  const test = (e) => {
+    console.log(e);
+  }
   return (
     <div className={moduleCss.newtodo} >
       <div className='left'>
-        <div>按钮</div>
-        <div>输入框</div>
+        <div className='btn show-btn'>
+          <Icon type='pp-big-circle' />
+        </div>
+        <div className='input'>
+          <Input
+            bordered={false}
+            style={{
+              height: '100%',
+              padding: '4px 0'
+            }}
+            placeholder='添加任务'
+            onPressEnter={test}
+          />
+        </div>
       </div>
       <div className='right'>
         配置器
