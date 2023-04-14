9ec0e44d2d4309f1c978b3f2850ab3920fc5fabe
zhaoxuepeng <1492548812@qq.com>
2023-04-14 16:28:33 +0800

    1

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@
 import { Navigate } from 'react-router-dom'
 import { lazy } from 'react'
 
-import Login from "@views/Login"
+const Login = lazy(() => import('@views/Login'))
 //懒加载
 const Register = lazy(() => import('@views/Register'))
 const Home = lazy(() => import('@views/Home'))
